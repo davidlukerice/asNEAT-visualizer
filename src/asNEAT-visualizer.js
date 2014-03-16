4c4220bbf5a9671d144712b7c754db71631079f1
David Rice <davidlukerice@gmail.com>
2014-03-16 14:28:05 -0500

    + added define() shim + updated index test page to load in libraries correctly

--- src/asNEAT-visualizer.js
+++ src/asNEAT-visualizer.js
@@ -1,4 +1,22 @@
 
 var Visualizer = {};
 
+/*
+  @param network {asNEAT.Network}
+*/
+Visualizer.createVisualization = function(defaultParameters) {
+  return new Visualizer.Visualization(defaultParameters);
+};
+
+var Visualization = function(defaultParameters) {
+  _.defaults(this, defaultParameters, this.defaultParameters);
+};
+Visualization.prototype.defaultParameters = {
+  network: null,
+  width: 800,
+  height: 600,
+  padding: 0
+};
+
+Visualizer.Visualization = Visualization; 
 export default Visualizer;

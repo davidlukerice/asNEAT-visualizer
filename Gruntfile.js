459c8ff549efde171eb988a297bca7e7b2ba33b5
David Rice <davidlukerice@gmail.com>
2014-03-16 02:04:13 -0500

    added es6 transpiling to tool chain

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,28 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['src/vendor/*.js','src/*.js', 'src/nodes/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
+
     jshint: {
       files: [
         'gruntfile.js',
@@ -39,6 +18,65 @@ module.exports = function(grunt) {
         }
       }
     },
+
+    clean: {
+      build: ['tmp']
+    },
+
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd:'src',
+          src: ['**/*.js'],
+          dest: 'tmp/javascript/'
+        }]
+      }
+    },
+
+    transpile: {
+      main: {
+        type: 'amd',
+        moduleName: function(path) {
+          return grunt.config.process('<%= pkg.name %>/') + path;
+        },
+        files: [{
+          expand: true,
+          cwd: 'tmp/javascript/',
+          src: ['**/*.js'],
+          dest: 'tmp/transpiled/'
+        }]
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/vendor/*.js', 'tmp/transpiled/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+
+
+
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    
     watch: {
       lint: {
         files: ['<%= jshint.files %>'],
@@ -65,12 +103,25 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-es6-module-transpiler');
 
   // task(s).
-  grunt.registerTask('test', ['jshint', 'qunit']);
-  grunt.registerTask('dist', ['jshint', 'concat', 'qunit', 'uglify']);
-  grunt.registerTask('watchLint', ['watch:lint']);
-  grunt.registerTask('watchDist', ['test', 'dist', 'concurrent:watches']);
-  grunt.registerTask('default', ['watchDist']);
+  //grunt.registerTask('test', ['jshint', 'qunit']);
+  //grunt.registerTask('dist', ['jshint', 'concat', 'qunit', 'uglify']);
+  //grunt.registerTask('watchLint', ['watch:lint']);
+  //grunt.registerTask('watchDist', ['test', 'dist', 'concurrent:watches']);
+  //grunt.registerTask('default', ['watchDist']);
+
+  grunt.registerTask('build', [
+    'jshint',
+    'qunit',
+    'clean:build',
+    'copy',
+    'transpile',
+    'concat',
+    'uglify'
+  ]);
 };
